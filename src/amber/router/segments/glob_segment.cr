220d2683ba093b8dbc4a58bf710cb9fd407f0f47
Sijawusz Pur Rahnama <sija@sija.pl>
2019-01-13 23:55:24 +0100

    Unescape * in GlobSegment(T) doc comment

--- src/amber/router/segments/glob_segment.cr
+++ src/amber/router/segments/glob_segment.cr
@@ -1,7 +1,7 @@
 module Amber::Router
   # Represents a "match anything" url segment.
   #
-  # In the url `/products/:23/&lowast;`, the first segment, `&lowast;` is a glob segment.
+  # In the url `/products/:23/*`, the first segment, `*` is a glob segment.
   class GlobSegment(T) < Segment(T)
     def match?(curious_segment : String)
       true
