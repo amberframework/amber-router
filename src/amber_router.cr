f1a80232f99c6ff656935da000c55702dee9d614
Isaac Sloan <isaac@isaacsloan.com>
2019-10-07 15:19:44 -0700

    Bumped version number to v0.4.0. (#21)

--- src/amber_router.cr
+++ src/amber_router.cr
@@ -1,8 +1,8 @@
 module Amber::Router
 end
 
-require "./amber/router/route_set"
-require "./amber/router/routed_result"
-require "./amber/router/segment"
-require "./amber/router/glob_match"
-require "./amber/router/parsers/*"
+require "./amber_router/route_set"
+require "./amber_router/routed_result"
+require "./amber_router/segment"
+require "./amber_router/glob_match"
+require "./amber_router/parsers/*"
